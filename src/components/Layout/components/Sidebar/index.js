7f44cc3c6f64dacd7120dae84c0124f41a19e85d
Luc Thuy <nguyenducminh01122002@gmail.com>
2023-03-27 00:21:55 +0700

    update sidebar

--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -1,19 +1,32 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faPenToSquare, faBook } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function SideBar() {
     return (
         <div className={cx('container', 'rounded')}>
-            <div>Ca nhan</div>
-            <ul>
-                <li>
-                    <Link to="/todaytodo">Today Todo</Link>
+            <div className={cx('top-section')}>
+                <button className={cx('rounded')}>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+                <span>Ca nhan</span>
+            </div>
+            <ul className={cx('link-list')}>
+                <li className={cx('rounded')}>
+                    <Link to="/todaytodo">
+                        <FontAwesomeIcon icon={faPenToSquare} />
+                        <span>Today Todo</span>
+                    </Link>
                 </li>
-                <li>
-                    <Link to="/diaryhistory">Diary History</Link>
+                <li className={cx('rounded')}>
+                    <Link to="/diaryhistory">
+                        <FontAwesomeIcon icon={faBook} />
+                        <span>Diary History</span>
+                    </Link>
                 </li>
             </ul>
             <div>Tao</div>
