b8c807789eea6698d453dc03a77fca06e90c6ff5
Luc Thuy <nguyenducminh01122002@gmail.com>
2023-03-29 00:05:28 +0700

    finish sidebar

--- src/components/Layout/components/Sidebar/index.js
+++ src/components/Layout/components/Sidebar/index.js
@@ -2,34 +2,80 @@ import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars, faPenToSquare, faBook } from '@fortawesome/free-solid-svg-icons';
+import {
+    faBars,
+    faPenToSquare,
+    faBook,
+    faUser,
+    faStore,
+} from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
 
 const cx = classNames.bind(styles);
 
 function SideBar() {
+    const [activeSidebar, setActiveSidebar] = useState(true);
+
     return (
-        <div className={cx('container', 'rounded')}>
-            <div className={cx('top-section')}>
-                <button className={cx('rounded')}>
-                    <FontAwesomeIcon icon={faBars} />
+        <div className={cx('container', 'rounded', { active: activeSidebar })}>
+            <div className={cx('section')}>
+                <button
+                    onClick={() => {
+                        setActiveSidebar(!activeSidebar);
+                    }}
+                    className={cx('rounded')}
+                >
+                    <FontAwesomeIcon icon={faBars} className={cx('icon')} />
                 </button>
-                <span>Ca nhan</span>
+                <Link to="/profile">
+                    <img
+                        src="https://cdn.lazi.vn/storage/uploads/users/cover/548014_1614328231.jpg"
+                        alt="avatar"
+                        className={cx('avatar', 'rounded', {
+                            active: activeSidebar,
+                        })}
+                    />
+                </Link>
+                <div className={cx('rounded', 'link')}>
+                    <Link to="/profile" className={cx('link-item')}>
+                        <FontAwesomeIcon icon={faUser} className={cx('icon')} />
+                        <span>Profile</span>
+                    </Link>
+                </div>
             </div>
-            <ul className={cx('link-list')}>
-                <li className={cx('rounded')}>
-                    <Link to="/todaytodo">
-                        <FontAwesomeIcon icon={faPenToSquare} />
-                        <span>Today Todo</span>
+            <div className={cx('section')}>
+                <ul>
+                    <li className={cx('rounded', 'link')}>
+                        <Link to="/todaytodo" className={cx('link-item')}>
+                            <FontAwesomeIcon
+                                icon={faPenToSquare}
+                                className={cx('icon')}
+                            />
+                            <span>Today Todo</span>
+                        </Link>
+                    </li>
+                    <li className={cx('rounded', 'link')}>
+                        <Link to="/diaryhistory" className={cx('link-item')}>
+                            <FontAwesomeIcon
+                                icon={faBook}
+                                className={cx('icon')}
+                            />
+                            <span>Diary History</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className={cx('section', 'bottom')}>
+                <div className={cx('rounded', 'link')}>
+                    <Link to="/smallstore" className={cx('link-item')}>
+                        <FontAwesomeIcon
+                            icon={faStore}
+                            className={cx('icon')}
+                        />
+                        <span>Small Store</span>
                     </Link>
-                </li>
-                <li className={cx('rounded')}>
-                    <Link to="/diaryhistory">
-                        <FontAwesomeIcon icon={faBook} />
-                        <span>Diary History</span>
-                    </Link>
-                </li>
-            </ul>
-            <div>Tao</div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 import { DefaultLayout } from '../components/Layout';
 import TodayTodo from '~/pages/TodayTodo';
-import DiaryHistory from '~/pages/DiaryHistory'
+import DiaryHistory from '~/pages/DiaryHistory';
+import Profile from '~/pages/Profile';
+import SmallStore from '~/pages/SmallStore';
 
 const publicRoutes = [
     { path: '/todaytodo', component: TodayTodo, layout: DefaultLayout },
     { path: '/diaryhistory', component: DiaryHistory, layout: DefaultLayout },
+    { path: '/profile', component: Profile, layout: DefaultLayout },
+    { path: '/smallstore', component: SmallStore, layout: DefaultLayout },
 ];
 
 const privateRoutes = [{}];
